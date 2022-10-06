5c0fab031fadc2a2f572c01626fab068d5b2ba10
AGUSTÍN SIROTINSKY <46645963@ort>
2022-10-06 08:22:52 -0300

    Create .Net MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace agussiro06_tp9.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
