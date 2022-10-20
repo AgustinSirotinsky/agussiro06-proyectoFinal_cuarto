e431a7f8de602fe7069183840359c58b5484ec6e
AGUSTÍN SIROTINSKY <46645963@ort>
2022-10-20 08:58:31 -0300

    Dia 20

---
+++ Models/BD.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace agussiro06_tp9.Models;
+
+public class BD 
+{
+    //CAMBIAR SERVIDOR 
+    private static string  _connectionstring = @"Server=A-PHZ2-CIDI-025;DataBase=ConsoleWorld;Trusted_Connection=True;";
+}
