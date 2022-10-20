70974dc7ab31b6cf51afca534c159f5dcb6de0f1
AGUSTÍN SIROTINSKY <46645963@ort>
2022-10-20 08:21:08 -0300

    Cambio lol

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Minijuego()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
