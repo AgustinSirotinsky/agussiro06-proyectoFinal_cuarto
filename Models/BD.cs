7cd8fb054fae982b08c26af21e42bfa3a7c396ca
AGUSTÍN SIROTINSKY <46645963@ort>
2022-10-25 12:06:28 -0300

    Base de datos avance

--- Models/BD.cs
+++ Models/BD.cs
@@ -8,5 +8,5 @@ namespace agussiro06_tp9.Models;
 public class BD 
 {
     //CAMBIAR SERVIDOR 
-    private static string  _connectionstring = @"Server=A-PHZ2-CIDI-025;DataBase=ConsoleWorld;Trusted_Connection=True;";
+    private static string  _connectionstring = @"Server=A-PHZ2-CIDI-029;DataBase=ConsoleWorld;Trusted_Connection=True;";
 }
